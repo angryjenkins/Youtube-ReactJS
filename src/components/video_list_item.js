dc08924a760c286c3d2a36c421363550ba2413c0
matt martin <matt@angryjenkins.com>
2019-12-21 17:43:45 -0500

    node 12.14 lts

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import moment from 'moment';
 
-const VideoListItem = ({video, onVideoSelect}) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
     // const video = props.video;
 
-    const parser = new DOMParser()
+    const parser = new DOMParser();
 
     const info = {
         image: video.snippet.thumbnails.default.url,
-        title: parser.parseFromString(`<!doctype html><body>${video.snippet.title}`,'text/html').body.textContent,
+        title: parser.parseFromString(
+            `<!doctype html><body>${video.snippet.title}`,
+            'text/html'
+        ).body.textContent,
         channel: video.snippet.channelTitle,
         date: moment(video.snippet.publishedAt).format('MM/YYYY')
-    }
+    };
 
     console.log(video);
 
     return (
-        <li 
-            onClick={() => onVideoSelect(video)} 
-            className="list-group-item videoSelect">
-            <div className="video-list media">
-                <div className="media-left">
-                    <img 
-                        className="media-object" 
-                        src={info.image} 
-                        alt={info.title} />
+        <li
+            onClick={() => onVideoSelect(video)}
+            className='list-group-item videoSelect'
+        >
+            <div className='video-list media'>
+                <div className='media-left'>
+                    <img
+                        className='media-object'
+                        src={info.image}
+                        alt={info.title}
+                    />
                 </div>
-                <div className="media-body">
-                    <div className="media-heading bold vidTitle">{info.title}</div>
-                    <p className="vidInfo">
-                        <em>{info.channel}, {info.date}</em>
+                <div className='media-body'>
+                    <div className='media-heading bold vidTitle'>
+                        {info.title}
+                    </div>
+                    <p className='vidInfo'>
+                        <em>
+                            {info.channel}, {info.date}
+                        </em>
                     </p>
                 </div>
             </div>
